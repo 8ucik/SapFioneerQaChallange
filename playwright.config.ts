197e3546f606b0f6b734c42989e5caaafbe4834f
8ucik <buciortomasz@gmail.com>
2025-07-08 17:51:42 +0200

    feat [add-runner] add run script and remove logs

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,12 +5,11 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
+    headless: true,
   },
-
   projects: [
     {
       name: 'chromium',
