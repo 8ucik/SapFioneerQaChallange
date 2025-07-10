16af2da9bd20fb4d962a596de69c7d7d8a49a24e
8ucik <buciortomasz@gmail.com>
2025-07-11 00:16:52 +0200

    improve time to multiply 5

--- pages/basePage.ts
+++ pages/basePage.ts
@@ -5,15 +5,17 @@ export class BasePage {
   protected page: Page;
   readonly timeout: number;
   readonly delay: number;
+  readonly multiply: number;
 
   constructor(page: Page) {
     this.page = page;
     this.timeout = 5000;
     this.delay = this.timeout / 10;
+    this.multiply = this.timeout * 5;
   }
 
   async navigateTo(url: string) {
-    await this.page.goto(url, { timeout: this.timeout * 1.5 });
+    await this.page.goto(url, { timeout: this.multiply });
   }
 
   async verifyPageTitle(title: RegExp | string) {
@@ -29,6 +31,6 @@ export class BasePage {
   }
 
   async verifyPageLink(url: string, timeout: number = this.timeout) {
-    await expect(this.page).toHaveURL(url, { ignoreCase: true, timeout: timeout * 1.5 });
+    await expect(this.page).toHaveURL(url, { ignoreCase: true, timeout: this.multiply });
   }
 }
